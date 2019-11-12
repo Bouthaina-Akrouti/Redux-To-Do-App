a51355d67d68cb175ace6cebd831b64384d0f801
bothazerty12 <bouthaynaakrouti522@gmail.com>
2019-11-12 16:47:57 +0100

    Updates

---
+++ precache-manifest.36746dae7fbb0137b6ac7cfbda1dd8d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bdb90c2d05968a6a442cab38693185a",
+    "url": "/Redux-To-Do-App/index.html"
+  },
+  {
+    "revision": "f3fc4bc58140db95d64c",
+    "url": "/Redux-To-Do-App/static/css/main.0cb12caf.chunk.css"
+  },
+  {
+    "revision": "11e77cbfbd188c5b2083",
+    "url": "/Redux-To-Do-App/static/js/2.c80cfee5.chunk.js"
+  },
+  {
+    "revision": "f3fc4bc58140db95d64c",
+    "url": "/Redux-To-Do-App/static/js/main.5d6deff3.chunk.js"
+  },
+  {
+    "revision": "a2c9095708342ba877ce",
+    "url": "/Redux-To-Do-App/static/js/runtime-main.fc3d602d.js"
+  }
+]);
